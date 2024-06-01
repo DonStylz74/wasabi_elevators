b804960923ba8744e02cd289ae8336229ac993c1
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-06-01 22:33:05 +1000

    Update config.lua

--- config.lua
+++ config.lua
@@ -33,4 +33,49 @@ Config.Elevators = {
             } -- Example without group
         },
     },
+PremiumCarsVespucci = { -- Elevator name(Doesn't show it's just to name table)
+        [1] = {
+            coords = vec3(-785.2402, -1044.3241, 14.1293), -- Coords, if you're new; last number is heading
+            heading = 294.8018, -- Heading of how will spawn on floor
+            title = 'Floor 1', -- Title 
+            description = 'Showroom Floor', -- Description
+            target = { -- Target length/width
+                width = 5,
+                length = 4
+            }
+        },
+        [2] = {
+            coords = vec3(-785.6124, -1044.2927, 20.1143), -- Coords, if you're new; last number is heading
+            heading = 294.8047,
+            title = 'Floor 2',
+            description = 'Office Floor',
+            target = {
+            width = 5,
+            length = 4
+            } -- Example without group
+        },
+    },
+PremiumCarsPillbox = { -- Elevator name(Doesn't show it's just to name table)
+        [1] = {
+            coords = vec3(-29.1420, -1094.1295, 26.4426), -- Coords, if you're new; last number is heading
+            heading = 69.6036, -- Heading of how will spawn on floor
+            title = 'Floor 1', -- Title 
+            description = 'Showroom Floor', -- Description
+            target = { -- Target length/width
+                width = 5,
+                length = 4
+            }
+        },
+        [2] = {
+            coords = vec3(-24.5921, -1106.5145, 35.9195), -- Coords, if you're new; last number is heading
+            heading = 73.7887,
+            title = 'Floor 2',
+            description = 'Office Floor',
+            target = {
+            width = 5,
+            length = 4
+            } -- Example without group
+        },
+    },
+
 }
